e2942670aca485afe14de081f73eb39feaf05413
Aniket9016 <nidhi14012patel@gamil.com>
2025-03-28 12:42:20 +0530

    First

---
+++ c#/IdentityBasedAuthSystem/IdentityBasedAuthSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentityBasedAuthSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
